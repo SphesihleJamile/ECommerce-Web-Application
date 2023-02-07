4f212418024238341383f71a3552507900f2c053
Sphesihle Jamile <sphesihlej@geotab.co.za>
2023-02-07 21:35:56 +0200

    Generic Method Complete

--- Ecommerce.DataAccess/Repositories/Abstract/IRepository.cs
+++ Ecommerce.DataAccess/Repositories/Abstract/IRepository.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ecommerce.DataAccess.Repositories.Abstract
 {
-    internal interface IRepository
+    internal interface IRepository<T> where T : class
     {
+        //Get All, Get By Id Firt or Default, Add, Remove, RemoveRange
+
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void Add(IEnumerable<T> entities);
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
     }
 }
---
+++ Ecommerce.DataAccess/Repositories/Concrete/Repository.cs
@@ -0,0 +1,60 @@
+﻿using Ecommerce.DataAccess.Data;
+using Ecommerce.DataAccess.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repositories.Concrete
+{
+    internal class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext dbContext;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbSet = dbContext.Set<T>(); // this wil bind our generic dbSet to the appropriate DbSet in the ApplicationDbContext class
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Add(IEnumerable<T> entities)
+        {
+            dbSet.AddRange(entities);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet; //we set is an an IQueryable<T> so that you can be able to make queries to it in the furture
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return query.ToList().FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
